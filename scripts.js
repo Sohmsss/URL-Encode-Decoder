53ce28e464e01bf7937b5f3daf1d2651f17ba197
Sohmsss <102739584+Sohmsss@users.noreply.github.com>
2024-01-30 20:50:52 +0000

    init

---
+++ scripts.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+
+    function toggleRadioButtons() {
+        var encodeToggle = document.getElementById('encode-toggle');
+        var decodeToggle = document.getElementById('decode-toggle');
+
+        encodeToggle.checked = true;
+        $('#btn-primary').text('Encode'); 
+
+        encodeToggle.addEventListener('change', function () {
+            if (this.checked) {
+                decodeToggle.checked = false;
+                $('.btn-primary').text('Encode'); 
+                $('#text-output').attr('placeholder', 'Encoded text will appear here')
+                $('#textinput').attr('placeholder', 'Text to be encoded')
+            }
+        });
+
+        decodeToggle.addEventListener('change', function () {
+            if (this.checked) {
+                encodeToggle.checked = false;
+                $('.btn-primary').text('Decode'); 
+                $('#textoutput').attr('placeholder', 'Decoded text will appear here')
+                $('#textinput').attr('placeholder', 'Text to be decoded')
+            }
+        });
+    }
+
+    toggleRadioButtons();
+
+    function processInput() {
+        var input = $('#textinput').val();
+        var isEncode = $('#encode-toggle').is(':checked');
+        var outputField = $('#textoutput');
+
+        if (isEncode) {
+            outputField.val(encodeURIComponent(input));
+        } else {
+            outputField.val(decodeURIComponent(input));
+        }
+    }
+
+    $('.btn-primary').on('click', function (e) {
+        e.preventDefault();
+        processInput();
+    });
+
+});
